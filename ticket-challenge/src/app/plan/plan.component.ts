9febf1583dd52065f644c8fa35053dd0937f5eb5
Saeed Bayat <01shadowalker01@gmail.com>
2025-10-04 23:10:30 +0330

    feat: load images and check for Canvas 2D support

--- ticket-challenge/src/app/plan/plan.component.ts
+++ ticket-challenge/src/app/plan/plan.component.ts
@@ -1,4 +1,5 @@
 import {
+  AfterViewInit,
   ChangeDetectionStrategy,
   Component,
   ElementRef,
@@ -14,10 +15,27 @@ const TOTAL_CELLS = GRID * GRID;
   styleUrls: ['./plan.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlanComponent {
+export class PlanComponent implements AfterViewInit {
   @ViewChild('canvas', { static: true })
   canvasRef!: ElementRef<HTMLCanvasElement>;
 
   // Compact state: 0 = free, 1 = reserved
   private state = new Uint8Array(TOTAL_CELLS);
+
+  private ctx!: CanvasRenderingContext2D;
+
+  private imgFreeSeat = new Image();
+  private imgFilledSeat = new Image();
+
+  ngAfterViewInit(): void {
+    this.imgFreeSeat.src = 'assets/icon/free-seat.svg';
+    this.imgFilledSeat.src = 'assets/icon/filled-seat.svg';
+
+    const canvas = this.canvasRef.nativeElement;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      throw new Error('Canvas 2D context not supported');
+    }
+    this.ctx = ctx;
+  }
 }
