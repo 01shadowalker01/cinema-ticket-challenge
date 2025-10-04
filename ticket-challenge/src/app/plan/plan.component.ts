7f3930dd0d26be9b7a1c2d9e81abf1eaab611d78
Saeed Bayat <01shadowalker01@gmail.com>
2025-10-04 22:58:56 +0330

    feat: implement basic canvas

--- ticket-challenge/src/app/plan/plan.component.ts
+++ ticket-challenge/src/app/plan/plan.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
+
+const GRID = 15; // 15x15
+const TOTAL_CELLS = GRID * GRID;
 
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
-  styleUrls: ['./plan.component.scss']
+  styleUrls: ['./plan.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PlanComponent implements OnInit {
+export class PlanComponent {
+  @ViewChild('canvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
 
-  constructor() { }
-
-  ngOnInit(): void {
-    // You should get the plan array from the: base-url/map/<map_id>
-  }
-
+  // Compact state: 0 = free, 1 = reserved
+  private state = new Uint8Array(TOTAL_CELLS);
 }
